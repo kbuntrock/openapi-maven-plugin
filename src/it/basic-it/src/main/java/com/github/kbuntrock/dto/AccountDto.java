69b0631524070efcabb239683735fd464426ff9c
kevin.buntrock@kleegroup.com <kevin.buntrock@kleegroup.com>
2022-04-09 19:42:49 +0200

    initial commit

---
+++ src/it/basic-it/src/main/java/com/github/kbuntrock/dto/AccountDto.java
@@ -0,0 +1,113 @@
+package com.github.kbuntrock.dto;
+
+
+import com.github.kbuntrock.Constants;
+
+import javax.validation.constraints.*;
+import java.util.Set;
+
+/**
+ * Object utilisé uniquement pour gérer l'identification d'un utilisateur côté client
+ */
+public class AccountDto {
+
+    @NotNull
+    @NotBlank
+    @Pattern(regexp = Constants.LOGIN_REGEX)
+    @Size(min = 1, max = 50)
+    private String login;
+
+    @NotNull
+    @Size(max = 50)
+    private String firstName;
+
+    @NotNull
+    @Size(max = 50)
+    private String lastName;
+
+    @NotNull
+    @Email
+    @Size(min = 5, max = 254)
+    private String email;
+
+    @Size(max = 256)
+    private String imageUrl;
+
+    @NotNull
+    private boolean activated = false;
+
+    @NotNull
+    @Size(min = 2, max = 10)
+    private String langKey;
+
+    @NotNull
+    private Set<String> authorities;
+
+    public AccountDto() {
+        // Empty constructor needed for Jackson.
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public boolean isActivated() {
+        return activated;
+    }
+
+    public void setActivated(boolean activated) {
+        this.activated = activated;
+    }
+
+    public String getLangKey() {
+        return langKey;
+    }
+
+    public void setLangKey(String langKey) {
+        this.langKey = langKey;
+    }
+
+    public Set<String> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Set<String> authorities) {
+        this.authorities = authorities;
+    }
+}
---
+++ src/test/java/com/github/kbuntrock/DocumentationMojoTest.java
@@ -0,0 +1,61 @@
+package com.github.kbuntrock;
+
+
+import org.apache.maven.plugin.testing.MojoRule;
+import org.apache.maven.plugin.testing.WithoutMojo;
+
+import org.junit.Rule;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.io.File;
+
+public class DocumentationMojoTest
+{
+    @Rule
+    public MojoRule rule = new MojoRule()
+    {
+        @Override
+        protected void before() throws Throwable 
+        {
+        }
+
+        @Override
+        protected void after()
+        {
+        }
+    };
+
+    /**
+     * @throws Exception if any
+     */
+    @Test
+    public void testSomething()
+            throws Exception
+    {
+        File pom = new File( "target/test-classes/project-to-test/" );
+        assertNotNull( pom );
+        assertTrue( pom.exists() );
+
+        DocumentationMojo documentationMojo = (DocumentationMojo) rule.lookupConfiguredMojo( pom, "documentation" );
+        assertNotNull(documentationMojo);
+        documentationMojo.execute();
+
+        File outputDirectory = ( File ) rule.getVariableValueFromObject(documentationMojo, "outputDirectory" );
+        assertNotNull( outputDirectory );
+        assertTrue( outputDirectory.exists() );
+
+        File touch = new File( outputDirectory, "touch.txt" );
+        assertTrue( touch.exists() );
+
+    }
+
+    /** Do not need the MojoRule. */
+    @WithoutMojo
+    @Test
+    public void testSomethingWhichDoesNotNeedTheMojoAndProbablyShouldBeExtractedIntoANewClassOfItsOwn()
+    {
+        assertTrue( true );
+    }
+
+}
+
