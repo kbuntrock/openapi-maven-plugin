9458c9e4b410ee6f9af0626c74b10931f2d53ee1
kevin.buntrock@kleegroup.com <kevin.buntrock@kleegroup.com>
2022-07-27 23:28:46 +0200

    Test IT in a different project

--- update-version.sh
+++ update-version.sh
@@ -3,7 +3,7 @@ read -p "Enter the new version number: " version
 mvn versions:set -DnewVersion="$version" -DgenerateBackupPoms=false;
 cd ./sample/sample-api;
 mvn versions:set-property -Dproperty=openapi-plugin-project-version -DnewVersion="$version" -DgenerateBackupPoms=false;
-cd ../../src/test/resources-its/com/github/kbuntrock/it;
+cd ../../openapi-maven-plugin/src/test/resources-its/com/github/kbuntrock/it;
 mvn versions:set-property -Dproperty=openapi-plugin-project-version -DnewVersion="$version" -DgenerateBackupPoms=false;
 
 read -p "All good, press enter to close the window";
