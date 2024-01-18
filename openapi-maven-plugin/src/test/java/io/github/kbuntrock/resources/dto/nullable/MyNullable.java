b97a3884fa7079258c90d4516e99cd8fb5f370a1
josve <josve@users.noreply.github.com>
2024-01-18 16:42:16 +0100

    Add test nullable annotation

---
+++ openapi-maven-plugin/src/test/java/io/github/kbuntrock/resources/dto/nullable/MyNullable.java
@@ -0,0 +1,9 @@
+package io.github.kbuntrock.resources.dto.nullable;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyNullable {
+}
