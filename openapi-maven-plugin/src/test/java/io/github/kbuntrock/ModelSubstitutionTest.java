654930cb127157448bb7c112b11b428843d253a6
kevin.buntrock@gmail.com <kevin.buntrock@gmail.com>
2024-02-02 16:41:08 +0100

    Json content directly in pom allowed

--- openapi-maven-plugin/src/test/java/io/github/kbuntrock/ModelSubstitutionTest.java
+++ openapi-maven-plugin/src/test/java/io/github/kbuntrock/ModelSubstitutionTest.java
@@ -28,10 +28,56 @@ public class ModelSubstitutionTest extends AbstractTest {
 
 		final DocumentationMojo mojo = new DocumentationMojo();
 		final ApiConfiguration apiConfiguration = new ApiConfiguration();
-		apiConfiguration.setOpenapiModelsPath(
+		apiConfiguration.setOpenapiModels(
 			"src/test/resources/ut/ModelSubstitutionTest/object_substitution/custom-openapi-model.yml");
-		apiConfiguration.setModelsAssociationsPath(
+		apiConfiguration.setModelsAssociations(
 			"src/test/resources/ut/ModelSubstitutionTest/object_substitution/custom-model-association.yml");
+		apiConfiguration.setAttachArtifact(false);
+		apiConfiguration.setLocations(Arrays.asList(ResponseEntityController.class.getCanonicalName()));
+		apiConfiguration.setTagAnnotations(Collections.singletonList(TagAnnotation.SPRING_MVC_REQUEST_MAPPING.getAnnotationClassName()));
+		mojo.setTestMode(true);
+		mojo.setApis(Collections.singletonList(apiConfiguration));
+		mojo.setProject(createBasicMavenProject());
+
+		final List<File> generated = mojo.documentProject();
+		checkGenerationResult("ut/ModelSubstitutionTest/object_substitution/object_substitution.yml", generated.get(0));
+	}
+
+	@Test
+	public void object_substitution_json_content() throws MojoFailureException, IOException, MojoExecutionException {
+
+		final DocumentationMojo mojo = new DocumentationMojo();
+		final ApiConfiguration apiConfiguration = new ApiConfiguration();
+		apiConfiguration.setOpenapiModels("{\n"
+			+ "  \"CustomAccountDto\": {\n"
+			+ "    \"description\": \"Custom version of the accountDto\",\n"
+			+ "    \"required\": [\n"
+			+ "      \"email\",\n"
+			+ "      \"token\"\n"
+			+ "    ],\n"
+			+ "    \"type\": \"object\",\n"
+			+ "    \"properties\": {\n"
+			+ "      \"email\": {\n"
+			+ "        \"type\": \"string\",\n"
+			+ "        \"format\": \"email\",\n"
+			+ "        \"minLength\": 5,\n"
+			+ "        \"maxLength\": 254\n"
+			+ "      },\n"
+			+ "      \"token\": {\n"
+			+ "        \"type\": \"number\",\n"
+			+ "        \"format\": \"double\"\n"
+			+ "      },\n"
+			+ "      \"activated\": {\n"
+			+ "        \"type\": \"boolean\"\n"
+			+ "      }\n"
+			+ "    }\n"
+			+ "  }\n"
+			+ "}");
+		apiConfiguration.setModelsAssociations("{\n"
+			+ "  \"equality\": {\n"
+			+ "    \"io.github.kbuntrock.resources.dto.AccountDto\": \"CustomAccountDto\"\n"
+			+ "  }\n"
+			+ "}");
 		apiConfiguration.setAttachArtifact(false);
 		apiConfiguration.setLocations(Arrays.asList(ResponseEntityController.class.getCanonicalName()));
 		apiConfiguration.setTagAnnotations(Collections.singletonList(TagAnnotation.SPRING_MVC_REQUEST_MAPPING.getAnnotationClassName()));
@@ -48,9 +94,9 @@ public class ModelSubstitutionTest extends AbstractTest {
 
 		final DocumentationMojo mojo = new DocumentationMojo();
 		final ApiConfiguration apiConfiguration = new ApiConfiguration();
-		apiConfiguration.setOpenapiModelsPath(
+		apiConfiguration.setOpenapiModels(
 			"src/test/resources/ut/ModelSubstitutionTest/enum_substitution/custom-openapi-model.yml");
-		apiConfiguration.setModelsAssociationsPath(
+		apiConfiguration.setModelsAssociations(
 			"src/test/resources/ut/ModelSubstitutionTest/enum_substitution/custom-model-association.yml");
 		apiConfiguration.setAttachArtifact(false);
 		apiConfiguration.setLocations(Arrays.asList(ResponseEntityController.class.getCanonicalName()));
