91476c29bc96723a9eb8327cea610758af465a3f
kevin.buntrock@gmail.com <kevin.buntrock@gmail.com>
2024-10-13 13:47:54 +0200

    Fix release

--- update-version.sh
+++ update-version.sh
@@ -7,7 +7,7 @@ then
 fi
 cd ./openapi-maven-plugin;
 mvn versions:set -DnewVersion="$version" -DgenerateBackupPoms=false;
-cd ../../openapi-maven-plugin/src/test/resources-its/io/github/kbuntrock/it;
+cd ../openapi-maven-plugin/src/test/resources-its/io/github/kbuntrock/it;
 mvn versions:set-property -Dproperty=openapi-plugin-project-version -DnewVersion="$version" -DgenerateBackupPoms=false;
 cd ../../../../../../../../integration-tests/src/test/resources-its/io/github/kbuntrock/it
 mvn versions:set-property -Dproperty=openapi-plugin-project-version -DnewVersion="$version" -DgenerateBackupPoms=false;
