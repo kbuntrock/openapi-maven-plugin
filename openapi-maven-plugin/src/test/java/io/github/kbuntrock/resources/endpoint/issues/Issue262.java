06c25dba08223bd5a666fbdcda5143bd6987e32d
kevin.buntrock <kevin.buntrock@gmail.com>
2025-11-15 20:05:05 +0100

    Disable generation as response of any class assignable by "org.springframework.web.servlet.view.AbstractView"

--- openapi-maven-plugin/src/test/java/io/github/kbuntrock/resources/endpoint/issues/Issue262.java
+++ openapi-maven-plugin/src/test/java/io/github/kbuntrock/resources/endpoint/issues/Issue262.java
@@ -2,11 +2,14 @@ package io.github.kbuntrock.resources.endpoint.issues;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.math.BigDecimal;
 
 @RequestMapping("/issue262")
 public interface Issue262 {
 
     @GetMapping("/some-redirection")
-    RedirectView redirectUsingRedirectView();
+    RedirectView redirectUsingRedirectView(@RequestParam int aNumber);
 }
