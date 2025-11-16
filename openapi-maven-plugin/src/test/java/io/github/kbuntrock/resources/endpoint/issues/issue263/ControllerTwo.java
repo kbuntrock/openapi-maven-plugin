64d43a66f40f41cb83e42fb05e0d7a2d02ce3f2f
kevin.buntrock <kevin.buntrock@gmail.com>
2025-11-16 10:41:36 +0100

    Follow up

--- openapi-maven-plugin/src/test/java/io/github/kbuntrock/resources/endpoint/issues/issue263/ControllerTwo.java
+++ openapi-maven-plugin/src/test/java/io/github/kbuntrock/resources/endpoint/issues/issue263/ControllerTwo.java
@@ -1,6 +1,7 @@
 package io.github.kbuntrock.resources.endpoint.issues.issue263;
 
 import io.github.kbuntrock.resources.dto.Authority;
+import io.github.kbuntrock.resources.dto.PageDto;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,10 +16,10 @@ public interface ControllerTwo {
      * @param evaluate
      * @param authority
      * @param file a file in the "checkFile" case
-     * @return a string in the "checkFile" case
+     * @return a PageDto of string in the "checkFile" case
      */
     @PostMapping(path = "dummy", consumes = { "multipart/form-data" })
-    String checkFile(@RequestParam(value = "evaluate", required = false) Boolean evaluate,
-                     @RequestParam(value = "authorityEnum", required = false) Authority authority,
-                     @RequestPart(value = "file", required = false) MultipartFile file);
+    PageDto<String> checkFile(@RequestParam(value = "evaluate", required = false) Boolean evaluate,
+                              @RequestParam(value = "authorityEnum", required = false) Authority authority,
+                              @RequestPart(value = "file", required = false) MultipartFile file);
 }
