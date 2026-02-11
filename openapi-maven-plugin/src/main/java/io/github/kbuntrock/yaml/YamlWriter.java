120094b4ed98d09c882cd5b82e6b42d94808c5e4
patred <giuseppe.patred@gmail.com>
2026-02-11 23:21:08 +0100

    use project description from POM in OpenAPI specs (#320)
    * retrieve description from pom
    
    * retrieve description from pom - fix test
    
    ---------
    
    Co-authored-by: patred <patred.job@gmail.com>
    Co-authored-by: Kévin Buntrock <kevin.buntrock@gmail.com>

--- openapi-maven-plugin/src/main/java/io/github/kbuntrock/yaml/YamlWriter.java
+++ openapi-maven-plugin/src/main/java/io/github/kbuntrock/yaml/YamlWriter.java
@@ -28,8 +28,19 @@ import org.apache.maven.project.MavenProject;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -117,7 +128,8 @@ public class YamlWriter {
 		}
 
 		final Specification specification = new Specification();
-		final Info info = new Info(context.getProject().getName(), context.getProject().getVersion(), freefields);
+		final Info info = new Info(context.getProject().getName(), context.getProject().getVersion(),
+			context.getProject().getDescription(), freefields);
 		specification.setInfo(info);
 
 		populateSpecificationFreeFields(specification, freefields);
